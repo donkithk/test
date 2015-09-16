855d079c8462c0ef20f44663f90fac24a8c1a8cb
KK <wankakit@cherrypicks.com>
2015-09-16 18:25:05 +0800

    testapps created

---
+++ TestApps/app/src/main/java/com/kkwan/testapps/MyActivity.java
@@ -0,0 +1,56 @@
+package com.kkwan.testapps;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+
+        TextView kkTV = (TextView) findViewById(R.id.kk01);
+        kkTV.setText("hello boba tea");
+
+        Button btnkk = (Button) findViewById(R.id.button);
+
+        btnkk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("KK TEST TAG", "You have clicked the button");
+                Intent intent = new Intent(MyActivity.this, MyActivity2.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_my, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
